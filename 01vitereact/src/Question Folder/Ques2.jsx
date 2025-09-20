af3f95a58b439a57e5153ad37b6b9c73b6244615
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-20 10:03:45 +0530

    Ques2 added

---
+++ 01vitereact/src/Question Folder/Ques2.jsx
@@ -0,0 +1,11 @@
+// Q2 (Props): Create a component Welcome that takes a name prop and displays “Welcome, [name]! .
+
+import React from "react";
+
+function welcome({name}){
+ return (
+<p>Hi! i am is {name} </p>
+
+ )
+}
+export default welcome;
